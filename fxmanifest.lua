1444e9bcaaf11a59103152bfc60fadbe2355b5c6
Samuel Santana <70180881+meliodax@users.noreply.github.com>
2021-03-29 12:32:31 -0300

    Add files via upload
    UPDATE 1.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,4 @@
-fx_version "adamant"
+ fx_version "adamant"
 game "gta5" 
 
 client_scripts {
--- server.lua
+++ server.lua
@@ -1,80 +1,68 @@
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- VRP
+-----------------------------------------------------------------------------------------------------------------------------------------
 local Tunnel = module("vrp","lib/Tunnel")
 local Proxy = module("vrp","lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP")
-
-sRP = {}
-Tunnel.bindInterface("vrp_roubos",sRP)
-
-vCLIENT = Tunnel.getInterface("vrp_roubos")
-
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- TUNNEL
+-----------------------------------------------------------------------------------------------------------------------------------------
+cO = {}
+Tunnel.bindInterface("core_roubos",cO)
+vCLIENT = Tunnel.getInterface("core_roubos")
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- VARIÁVEIS
+-----------------------------------------------------------------------------------------------------------------------------------------
 local ultimoAssaltoHora = {}
+local checkRobbery = {}
 local recompensa = {}
-local assalto = {}
-local tempoAssalto = {}
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- STARTROBBERY
+-----------------------------------------------------------------------------------------------------------------------------------------
+function cO.startRobbery(rJs,sJs)
+    local source = source 
+    local user_id = vRP.getUserId(source) 
+    if user_id then 
+        local vector = json.decode(rJs)
+        local setup = json.decode(sJs)
+        for k,v in pairs(setup) do 
+            if k == vector.type then 
+                local amountCops = vRP.getUsersByPermission("policia.permissao")
+                if parseInt(#amountCops) <= parseInt(v.lspd) then
+                    TriggerClientEvent("Notify",source,"aviso","Sistema indisponível no momento, tente mais tarde.",5000)
+                    return false
+                end
 
-local policias = nil
-
-function sRP.startRobbery(rouboJson, setupJson)
-    local source = source
-    local user_id = vRP.getUserId(source)
-    if user_id then
-        local v = json.decode(rouboJson)
-        local setup = json.decode(setupJson)    
-        for k,c in pairs(setup) do
-            Wait(1)
-            if k == v.type then
-                policias = vRP.getUsersByPermission("policia.permissao")
-                if not vRP.searchReturn(source,user_id) then
-                    if #policias < c.lspd then
-                        TriggerClientEvent("Notify",source,"negado", "Necessários "..c.lspd.." <b>Políciais</b> para iniciar o roubo.")
-                    else
-                        if sRP.isEnabledToRob(k,v["id"], c.tempoEspera) then
-                            if sRP.hasNecessaryItemsToRob(user_id, c) then
-
-                            -- TODO: Implementar sistemas da checklist...
-                            local time = c.tempoEspera * 60 / 2
-                            vRP.searchTimer(user_id,parseInt(time))
-                            ultimoAssaltoHora[v["id"]] = os.time()
-                            recompensa[user_id] = c
-                            tempoAssalto[user_id] = c.tempo
-                            assalto[k] = true
-
-
-                            SetTimeout(c.tempo * 1000,function()
-                                assalto[k] = false
-                            end)
-                
-                            vRPclient._playAnim(source,false,{"anim@heists@ornate_bank@grab_cash_heels","grab"},true)
-
-                            vCLIENT.iniciandoRoubo(source,v.x, v.y, v.z, c.tempo, v.h)
-                                
-                            sRP.avisarPolicia("Roubo em Andamento", "Assalto a "..v.type.." em andamento, verifique o ocorrido.", v.x, v.y, v.z, v.type)
-                            else
-                                TriggerClientEvent("Notify", source, "sucesso", "Você não conseguiu abrir o cofre, é melhor correr!")
-                            end
-                        else
-                            local tempoRestante = sRP.getRemaningTime(k,v["id"], c.tempoEspera)
-                            TriggerClientEvent("Notify", source, "sucesso", "Você ainda deve aguardar "..tempoRestante.." segundos para realizar a ação.")
-                        end
+                local shopActived = vector.type..vector.id
+                if cO.isEnabledToRob(k,shopActived, v.tempoEspera) then
+                    if cO.hasNecessaryItemsToRob(user_id, v) then
+                        vRP.searchTimer(user_id,parseInt(v.tempoEspera * 60 / 2))
+                        ultimoAssaltoHora[shopActived] = os.time()
+                        checkRobbery[source] = shopActived
+                        recompensa[user_id] = v
+                        vRPclient._playAnim(source,false,{"anim@heists@ornate_bank@grab_cash_heels","grab"},true)
+                        vCLIENT.iniciandoRoubo(source,vector.x, vector.y, vector.z, vector.tempo, vector.h)                                
+                        cO.avisarPolicia("Roubo em Andamento", "Assalto a "..vector.type.." em andamento, verifique o ocorrido.", vector.x, vector.y, vector.z, vector.type)
                     end
+                else
+                    local tempoRestante = cO.getRemaningTime(k,shopActived, v.tempoEspera)
+                    TriggerClientEvent("Notify", source, "sucesso", "Você ainda deve aguardar "..tempoRestante.." segundos para realizar a ação.")
                 end
             end
         end
     end
 end
-
-function sRP.cancelRobbery()
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- CANCEL ROBBERY
+-----------------------------------------------------------------------------------------------------------------------------------------
+function cO.cancelRobbery()
     local source = source
     local user_id = vRP.getUserId(source)
 
     if user_id then
-        for n,i in pairs(recompensa[user_id].items) do
-            local pagamento = math.random(i.min,i.max)
-            vRP.giveInventoryItem(user_id,n,parseInt(pagamento)/10,true)
-        end
-
-        tempoAssalto[user_id] = nil
+        ultimoAssaltoHora[checkRobbery[source]] = nil
+        checkRobbery[source] = nil
         recompensa[user_id] = nil
         local policia = vRP.getUsersByPermission("policia.permissao")
         for l,w in pairs(policia) do
@@ -88,57 +76,63 @@ function sRP.cancelRobbery()
 		end
     end
 end
-
-function sRP.getRemaningTime(k, user_id,tempoEspera)
-    local t = ((os.time() - ultimoAssaltoHora[user_id]) - tempoEspera * 60) * -1
-    return t
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- GETREMAININGTIME
+-----------------------------------------------------------------------------------------------------------------------------------------
+function cO.getRemaningTime(_,roubo,cooldown)
+    local timing = ((os.time() - ultimoAssaltoHora[roubo]) - cooldown * 60) * -1
+    return timing
 end
-
-function sRP.isEnabledToRob(k, user_id, tempoEspera)
-    if ultimoAssaltoHora[user_id] then
-        if (os.time() - ultimoAssaltoHora[user_id]) < tempoEspera * 60 then
-            return false
-        else
-            return true
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- CHECK
+-----------------------------------------------------------------------------------------------------------------------------------------
+function cO.isEnabledToRob(_,roubo,cooldown)
+    if ultimoAssaltoHora[roubo] then 
+        if (os.time() - ultimoAssaltoHora[roubo]) < cooldown * 60 then 
+            return false 
         end
     end
-    return true
+    return true 
 end
-
-function sRP.givePayment()
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- GIVEPAYMENT
+-----------------------------------------------------------------------------------------------------------------------------------------
+function cO.givePayment()
     local source = source 
     local user_id = vRP.getUserId(source)
     if user_id then
-        for n,i in pairs(recompensa[user_id].items) do
-            local pagamento = math.random(i.min,i.max)
-            vRP.giveInventoryItem(user_id,n,parseInt(pagamento),true)
+        for k,v in pairs(recompensa[user_id].items) do 
+            vRP.giveInventoryItem(user_id,k,parseInt(math.random(i.min,i.max)))
         end
         recompensa[user_id] = nil
-        tempoAssalto[user_id] = nil
+        ultimoAssaltoHora[checkRobbery[source]] = nil
+        checkRobbery[source] = nil
     end
 end
-
-function sRP.hasNecessaryItemsToRob(user_id, c)
-    local source = vRP.getUserSource(user_id)
-    local pica = json.encode(c.itemsNecessarios)
-    local picapica2 = json.decode(pica)
-    if c.itemsNecessarios then
-        local itensNecessarios = #c.itemsNecessarios
-        local count = 0
-        for k,v in pairs(c.itemsNecessarios) do
-            if vRP.getInventoryItemAmount(user_id,k) >= parseInt(v.qtd) then 
-                vRP.tryGetInventoryItem(user_id,k,v.qtd,true)
-                return true 
-            else
-                TriggerClientEvent("Notify",source, "sucesso", "Você precisa de "..v.qtd.."x <b>"..vRP.itemNameList(k).."</b> para iniciar")
-                return false
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- CHECK ITEMS
+-----------------------------------------------------------------------------------------------------------------------------------------
+function cO.hasNecessaryItemsToRob(user_id, c)
+    local source = vRP.getUserSource(parseInt(user_id))
+    if source then 
+        if c.itemsNecessarios then
+            for k,v in pairs(c.itemsNecessarios) do
+                if vRP.getInventoryItemAmount(user_id,k) >= parseInt(v.qtd) then 
+                    vRP.tryGetInventoryItem(user_id,k,v.qtd,true)
+                    return true 
+                else
+                    TriggerClientEvent("Notify",source, "sucesso", "Você precisa de "..v.qtd.."x <b>"..exports["mlx-inventory"]:itemNameList(k).."</b> para iniciar")
+                    return false
+                end
             end
         end
     end
     return true
 end
-
-function sRP.avisarPolicia(titulo, msg, x, y, z, name)
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- CALL POLICE
+-----------------------------------------------------------------------------------------------------------------------------------------
+function cO.avisarPolicia(titulo, msg, x, y, z, name)
 	for l,w in pairs(policias) do
 		local player = vRP.getUserSource(parseInt(w))
 		if player then
